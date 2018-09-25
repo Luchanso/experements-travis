abace21679e33d8bba3cc6415138a5c6404fd42b
Luchanskiy Oleg <luchansodev@gmail.com>
2018-09-25 13:13:18 +0300

    fix

--- index.test.js
+++ index.test.js
@@ -1,9 +1,5 @@
 describe('check CI server information', () => {
     it('should return process information', () => {
-        console.log(process);
-        console.log(os);
-
         expect(process).not.toBeNull();
-        expect(os).not.toBeNull();
     });
 })
